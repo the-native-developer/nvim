647c2105331d47ce0f9de311cec1204999b782c7
Mathias Lüer <mathias.luer@gmail.com>
2022-07-30 21:04:07 +0200

    change nerdtree keymaps

--- after/plugin/nerdtree.lua
+++ after/plugin/nerdtree.lua
@@ -13,6 +13,5 @@ vim.g['NERDTreeGitStatusIndicatorMapCustom'] = {
 }
 
 vim.api.nvim_set_keymap('n', '<leader>n',':NERDTreeFocus<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-n>', ':NERDTree<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-p>', ':NERDTreeFind<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>nt', ':NERDTreeToggle<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>nf', ':NERDTreeFind<CR>', { noremap = true })
