5e9b26a67907dc210a9aaba338d9a6bed25666a1
Mathias Lüer <mathias.luer@gmail.com>
2022-07-30 21:21:50 +0200

    add keymap for telescope diagnostics

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -100,6 +100,7 @@ telescope.setup {
 -- Using Lua functions
 vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fd', "<cmd>lua require('telescope.builtin').diagnostics()<cr>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fh', "<cmd>lua require('telescope.builtin').oldfiles()<cr>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fhh', "<cmd>lua require('telescope.builtin').help_tags()<cr>", { noremap = true })
