33acb7c50ff2185e6e389014d5ca2006e04de8f2
Mathias Lüer <mathias.luer@gmail.com>
2022-08-28 23:06:55 +0200

    improve winbar in lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -18,7 +18,7 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {'branch'},
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
@@ -27,8 +27,8 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = {},
+    lualine_x = {},
     lualine_y = {},
     lualine_z = {}
   },
@@ -39,16 +39,54 @@ require('lualine').setup {
     lualine_b = {},
     lualine_c = {},
     lualine_x = {},
-    lualine_y = {'diagnostics'},
-    lualine_z = {'filename'}
+    lualine_y = {'diff','diagnostics'},
+    lualine_z = {
+        {
+          'filename',
+          file_status = true,      -- Displays file status (readonly status, modified status)
+          newfile_status = false,  -- Display new file status (new file means no write after created)
+          path = 1,                -- 0: Just the filename
+                                   -- 1: Relative path
+                                   -- 2: Absolute path
+                                   -- 3: Absolute path, with tilde as the home directory
+
+          shorting_target = 10,    -- Shortens path to leave 40 spaces in the window
+                                   -- for other components. (terrible name, any suggestions?)
+          symbols = {
+            modified = '[+]',      -- Text to show when the file is modified.
+            readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+            unnamed = '[No Name]', -- Text to show for unnamed buffers.
+            newfile = '[New]',     -- Text to show for new created file before first writting
+          }
+        }
+     }
   },
   inactive_winbar = {
     lualine_a = {},
     lualine_b = {},
     lualine_c = {},
     lualine_x = {},
-    lualine_y = {'diagnostics'},
-    lualine_z = {'filename'}
+    lualine_y = {'diff','diagnostics'},
+    lualine_z = {
+        {
+          'filename',
+          file_status = true,      -- Displays file status (readonly status, modified status)
+          newfile_status = false,  -- Display new file status (new file means no write after created)
+          path = 1,                -- 0: Just the filename
+                                   -- 1: Relative path
+                                   -- 2: Absolute path
+                                   -- 3: Absolute path, with tilde as the home directory
+
+          shorting_target = 10,    -- Shortens path to leave 40 spaces in the window
+                                   -- for other components. (terrible name, any suggestions?)
+          symbols = {
+            modified = '[+]',      -- Text to show when the file is modified.
+            readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+            unnamed = '[No Name]', -- Text to show for unnamed buffers.
+            newfile = '[New]',     -- Text to show for new created file before first writting
+          }
+        }
+     }
   },
   extensions = {}
 }
