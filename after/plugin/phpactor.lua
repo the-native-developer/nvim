2d0759c4a9ecd601cc064a1fa36bd9f675ec3b2c
Mathias Lüer <mathias.luer@gmail.com>
2023-02-13 07:07:55 +0100

    modernize phpactor

--- after/plugin/phpactor.lua
+++ after/plugin/phpactor.lua
@@ -14,10 +14,25 @@
             options = {},
         },
     })
-    local opt = {remap = false}
-    vim.keymap.set('n', '<leader>pt', '<cmd>PhpActor transform<CR>', opt)
-    vim.keymap.set('n', '<leader>pc', '<cmd>PhpActor context_menu<CR>', opt)
-    vim.keymap.set('n', '<leader>pga', '<cmd>PhpActor generate_accessor<CR>', opt)
-    vim.keymap.set('n', '<leader>pcv', '<cmd>PhpActor change_visibility<CR>', opt)
-    vim.cmd 'autocmd FileType php set iskeyword+=$'
+
+    vim.api.nvim_create_augroup('setPhpActorShortCuts', { clear = true })
+    vim.api.nvim_create_autocmd('FileType', {
+        group = 'setPhpActorShortCuts',
+        pattern = { 'php' },
+        callback = function ()
+            local opt = {remap = false}
+            vim.keymap.set('n', '<leader>pt', '<cmd>PhpActor transform<CR>', opt)
+            vim.keymap.set('n', '<leader>pc', '<cmd>PhpActor context_menu<CR>', opt)
+            vim.keymap.set('n', '<leader>pga', '<cmd>PhpActor generate_accessor<CR>', opt)
+            vim.keymap.set('n', '<leader>pcv', '<cmd>PhpActor change_visibility<CR>', opt)
+        end
+    })
+
+    vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('setPhpKeyword', { clear = true }),
+        pattern = { 'php' },
+        callback = function ()
+            vim.opt.iskeyword:append('$')
+        end
+    })
 -- end
