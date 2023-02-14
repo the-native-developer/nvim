8bbb3d003c9b981fef480376e547f29fc47d5e61
Mathias Lüer <mathias.luer@gmail.com>
2023-02-14 22:07:09 +0100

    fix javascript identation

--- lua/matt/set.lua
+++ lua/matt/set.lua
@@ -4,6 +4,25 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
+vim.api.nvim_create_autocmd('FileType', {
+    group    = vim.api.nvim_create_augroup('vue', {clear=true}),
+    pattern  = {
+        'javascript',
+        'javascript.jsx',
+        'typescript',
+        'html',
+        'css',
+        'less',
+        'vue'
+    },
+    callback = function ()
+        vim.opt.tabstop = 2
+        vim.opt.softtabstop = 2
+        vim.opt.shiftwidth = 2
+    end,
+})
+
 vim.opt.expandtab = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
