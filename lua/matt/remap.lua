ee9184d25db617e58ed8ec6d9112e239953f1158
Mathias Lüer <mathias.luer@gmail.com>
2023-02-18 22:52:07 +0100

    improve remaps

--- lua/matt/remap.lua
+++ lua/matt/remap.lua
@@ -2,28 +2,31 @@ vim.g.mapleader = ' '
 
 local opt = { noremap = true }
 --Remove newbie crutches in Insert Mode
-vim.api.nvim_set_keymap('i', '<Down>', '<Nop>', opt)
-vim.api.nvim_set_keymap('i', '<Left>', '<Nop>', opt)
-vim.api.nvim_set_keymap('i', '<Right>', '<Nop>', opt)
-vim.api.nvim_set_keymap('i', '<Up>', '<Nop>', opt)
+vim.keymap.set('i', '<Down>', '<Nop>', opt)
+vim.keymap.set('i', '<Left>', '<Nop>', opt)
+vim.keymap.set('i', '<Right>', '<Nop>', opt)
+vim.keymap.set('i', '<Up>', '<Nop>', opt)
 
 -- Remove newbie crutches in Normal Mode
-vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', opt)
-vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', opt)
-vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', opt)
-vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', opt)
+vim.keymap.set('n', '<Down>', '<Nop>', opt)
+vim.keymap.set('n', '<Left>', '<Nop>', opt)
+vim.keymap.set('n', '<Right>', '<Nop>', opt)
+vim.keymap.set('n', '<Up>', '<Nop>', opt)
 
 -- Remove newbie crutches in Visual Mode
-vim.api.nvim_set_keymap('v', '<Down>', '<Nop>', opt)
-vim.api.nvim_set_keymap('v', '<Left>', '<Nop>', opt)
-vim.api.nvim_set_keymap('v', '<Right>', '<Nop>', opt)
-vim.api.nvim_set_keymap('v', '<Up>', '<Nop>', opt)
+vim.keymap.set('v', '<Down>', '<Nop>', opt)
+vim.keymap.set('v', '<Left>', '<Nop>', opt)
+vim.keymap.set('v', '<Right>', '<Nop>', opt)
+vim.keymap.set('v', '<Up>', '<Nop>', opt)
 
 -- source current file
-vim.api.nvim_set_keymap('n', '<leader><leader>x', '<cmd>source %<cr>', opt)
+vim.keymap.set('n', '<leader><leader>x', '<cmd>source %<cr>', opt)
+
+--paste without overwriting clipboard
+vim.keymap.set('n', '<leader>p', '"_dP', opt)
 
 -- open terminal below all splits
-vim.api.nvim_set_keymap('n', '<leader>to', '<cmd>sp<CR><cmd>bo term<CR><C-w><S-k>i', opt)
+vim.keymap.set('n', '<leader>to', '<cmd>sp<CR><cmd>bo term<CR><C-w><S-k>i', opt)
 
 -- remap page up and down to be centered on screen
 vim.keymap.set('n', '<C-d>', '<C-d>zz', opt)
@@ -33,4 +36,4 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz', opt)
 vim.keymap.set('n', '<leader>ex', vim.cmd.Explore, opt)
 
 -- remap brakets to english keyboard style
-vim.o.langmap="ü+Ü*;[]{}"
+vim.o.langmap="öäÖÄ;[]{}"
