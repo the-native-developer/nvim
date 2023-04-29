131f891de679f38ad1de2b42978c5c801a8843e6
Mathias Lüer <mathias.lueer@etailer.de>
2023-04-29 16:39:41 +0200

    install tmux-navigator

--- lua/matt/packer.lua
+++ lua/matt/packer.lua
@@ -20,6 +20,28 @@ return require('packer').startup(function(use)
       requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
 
+    -- harpoon 
+    use { 'ThePrimeagen/harpoon',
+      requires = { 'nvim-lua/plenary.nvim' }
+    }
+
+    use { 'alexghergh/nvim-tmux-navigation', config = function()
+
+            local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+            nvim_tmux_nav.setup {
+                disable_when_zoomed = true -- defaults to false
+            }
+
+            vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+            vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+            vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+            vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+            vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+            vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+
+        end
+    }
     -- Color theme plugins
     use {'dracula/vim', as =  'dracula' }
     use 'morhetz/gruvbox'
